b277ea129a05e76f0ecab256a486fe51c09c6962
Ian Walter <public@iankwalter.com>
2019-10-29 17:15:04 -0400

    Updating cli

--- cli.js
+++ cli.js
@@ -4,20 +4,33 @@ const cli = require('@ianwalter/cli')
 const { print } = require('@ianwalter/print')
 const { oneLine } = require('common-tags')
 const { generateRepo } = require('.')
+const { description } = require('./package.json')
 
 async function run () {
-  const { _: [dir], $package, ...config } = cli({ 
+  const { _: [dir], packageJson, ...config } = cli({
     name: 'faygit',
-    opts: {
-      alias: {
-        force: 'f'
+    description,
+    usage: 'faygit <directory>',
+    options: {
+      force: {
+        alias: 'f',
+        description: `
+          Whether to re-initialize git if the given directory is already a git
+          repository
+        `,
+        default: false
       }
     }
   })
-  const { numberOfCommits } = await generateRepo({ dir, ...config })
-  print.success(oneLine`
-    Added ${numberOfCommits} commit${numberOfCommits > 1 ? 's' : ''}!
-  `)
+
+  if (config.help) {
+    print.info(config.help)
+  } else {
+    const { numberOfCommits } = await generateRepo({ dir, ...config })
+    print.success(oneLine`
+      Added ${numberOfCommits} commit${numberOfCommits > 1 ? 's' : ''}!
+    `)
+  }
 }
 
 run().catch(err => print.error(err))
